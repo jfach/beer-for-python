0cbc0291c44415cd669ab544d59c4fc56ea70c21
Jordan Facibene <jordan.facibene13@stjohns.edu>
2015-04-09 23:24:13 -0400

    more PyPI stuff..

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
 	name="pybeer",
 
 	# Version number:
-	version="0.1.23",
+	version="0.1.24",
 	description="Access quality beer data through Python.",
 	# Application author details:
 	author="Jordan Facibene",
 	author_email="jordan.facibene13@stjohns.edu",
-	download_url = 'https://github.com/Jfach/beer-for-python/tarball/0.1.23',
+	download_url = 'https://github.com/Jfach/beer-for-python/tarball/0.1.24',
 
 	# Packages
 	packages=["pybeer"],
