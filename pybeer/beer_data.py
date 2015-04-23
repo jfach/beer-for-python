5afe91edea5daddb6bd574dcc19ae3e039e3e413
Harry Vane <harryavane@gmail.com>
2015-04-23 18:33:57 +0100

    Goodbye mechanize
    - no longer use mechanize
    - we now use regex to find links on the search page

--- pybeer/beer_data.py
+++ pybeer/beer_data.py
@@ -1,27 +1,39 @@
-import mechanize
+import re
+import requests
 import bad_beer
 
-search = mechanize.Browser()
-search.addheaders = [("User-agent", 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)')]
+main_url = "http://www.beeradvocate.com"
+beer_url_re = re.compile(r'/beer/profile/[0-9]+/[0-9]+/?')
 
 def generate_link(beer):
-	beer_name = beer.replace(" ", "+")
-	query = "http://www.beeradvocate.com/search/?q={}&qt=beer".format(beer_name)
-	return query
+    beer_name = beer.replace(" ", "+")
+    query = main_url + "/search/?q={}&qt=beer".format(beer_name)
+    
+    return query
 
 def find_beer_link(beer):
-	url = generate_link(beer)
-	search.open(url)
-	for link in search.links():
-		if str(link.text).lower() == beer.lower():
-			return link.url
-	raise bad_beer.Invalid_Beer("ERROR: [{}] not found. Did you check the spelling/name?".format(beer))
+    url = generate_link(beer)
+
+    response = requests.get(url)
+
+    links = beer_url_re.findall(response.text)
+
+    #links is either empty, or a list of valid links
+    #how do we decide which one? Do we assume the first one?
+    #That's what we'll do for now (TODO)
+
+    if links:
+        return main_url + links[0]
+
+    raise bad_beer.Invalid_Beer("ERROR: [{}] not found. "
+                "Did you check the spelling/name?".format(beer))
 
 def beer_profile(beer):
-	beer_page = find_beer_link(beer)
-	response = search.open(beer_page)
-	raw = response.read()
-	return raw
+    beer_page = find_beer_link(beer)
+    response = requests.get(beer_page)
+    
+    #should we soup up here? It would mean importing BS just for one call
+    return response.text
 
 
 
